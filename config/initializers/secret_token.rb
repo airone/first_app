6ed2be7048f6f214f67b4668476c1e6c80fb22ec
mobapp1 <mobapp1@gmail.com>
2011-07-01 21:16:30 +0200

    super commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3d9f0588600f943561bf471a334e3e073b42a5267e73825460967762035c086cfa34060ac3291c7ab0b606ea55b51a6b2db4b37c858f0705c54fe59eec19d241'
